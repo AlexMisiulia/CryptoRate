8371a9437edea5f27cb97c952c9c7946e65eb1fa
alex <alexeymisiulia@gmail.com>
2018-01-10 15:34:56 +0300

    initial commit

---
+++ app/src/main/java/best/lang/cryptorates/MainActivity.kt
@@ -0,0 +1,12 @@
+package best.lang.cryptorates
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
